51c7459679d672791727eedcaabdd05d33ec2ba7
Francesca Palumbo <econofra87@gmail.com>
2020-05-21 17:22:57 +0200

    senza bonus

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+//  Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
+// Ciclare quindi i dischi ottenuti e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+
 $(document).ready(function() {
 
+    // Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
     // faccio la chiamata ajax
     $.ajax({
         'url' : 'https://flynn.boolean.careers/exercises/api/array/music' ,
@@ -10,24 +14,36 @@ $(document).ready(function() {
             var album = data.response;
             console.log(album);
 
+            var html_template   = $("#cd-template").html();
+            var template_function = Handlebars.compile(html_template);
+
+            // Ciclare quindi i dischi e ottenuti
             // apro un ciclo for-in  per accedere a tutte le proprietà dell'oggetto
-            for (var key in album ){
-                console.log(album [key]);
-                $(album).text();
-            };
-        };
+            for (var i = 0; i < album.length ; i++) {
+                var album_corrente = album[i];
+                var context = {
+    					poster: album_corrente.poster,
+    					title: album_corrente.title,
+    					author: album_corrente.author,
+    					year: album_corrente.year,
+                        genre: album_corrente.genre
+    				};
+                console.log(album_corrente.author);
+
+                var clone_html = template_function(context);
+                $('.cds-container').append(clone_html);
+
+            }
+        },
 
         'error' : function(){
             console.log('si è verificato un errore');
         }
     });
 
+    // siccome voglio visualizzare la variabile 'album' in pagina, creo una funzione che richiama dall'esterno la risposta ajax
+    function stampa_album(album){
+        // console.log(album);
+        // $('template-cards').append(album);
+    }
 });
-
-// HANDLEBARS-Template
-// disegnare in pagina una card utilizzando handlebars.
-
-var html_template   = $("#template-cards").html();
-var template_function = Handlebars.compile(html_template);
-
-function
